a123022bf7c8300bc5b3eee935b09b00e0d994dd
BrainCellsoptional <olivernicholls27@gmail.com>
2025-10-18 15:33:50 +1300

    Deploy React app

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: "/Semester-3-Comp6210-Web-services-And-Design-Methodologies-Assignment-2-React-SupaBase/",
-  plugins: [react()],
-})
