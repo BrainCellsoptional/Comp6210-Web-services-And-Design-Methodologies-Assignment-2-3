7802b4d5bb7faf9a40a9cf84cade7e1095398de3
BrainCellsoptional <olivernicholls27@gmail.com>
2025-10-18 16:18:44 +1300

    Comment Text

--- src/components/Highlight.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-export default function Highlight({ text }) {
-  const highlighted = text
-    .replace(/Euclid/gi, '<span style="color: orange; font-weight: bold;">Euclid</span>')
-    .replace(/Safe/gi, '<span style="color: green; font-weight: bold;">Safe</span>')
-    .replace(/Keter/gi, '<span style="color: red; font-weight: bold;">Keter</span>');
-
-  return <div dangerouslySetInnerHTML={{ __html: highlighted }} />;
-}
