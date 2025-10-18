8ab47b3d75ab569e71155af9774c5fa899cfff0b
BrainCellsoptional <olivernicholls27@gmail.com>
2025-10-18 15:31:19 +1300

    first commit

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: "/Semester-3-Comp6210-Web-services-And-Design-Methodologies-Assignment-2-React-SupaBase/",
+  plugins: [react()],
+})
